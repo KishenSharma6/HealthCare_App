3b25d3c46adc61d7b02da224804c83aed13826f4
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-08-26 19:00:47 -0700

    Added base bmi function check

--- HealthApp/main.cpp
+++ HealthApp/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#define inch_to_cm(x)(x * 2.54)
+#define inch_to_cm(x)((x * 2.54) /100)
 #define lb_to_kg(x)(x / 2.20462)
 #define patient_bmi(kg, meters)(kg/(meters * meters))
 
@@ -12,18 +12,32 @@ float bmi;
 float meters;
 float kg;
 
+void isPatientHealthy(float bmi){
+    if (bmi >= 30){
+        cout<< "Patient is obese" << endl;
+    } else if (bmi < 30 and bmi >=25)
+    {
+        cout<< "Patient is overweight" <<endl;
+    } else if (bmi <25)
+    {
+        cout<<"Patient is healthy" << endl;
+    }
+    
+    
+
+};
 int main(){
     cout << "Input patient height in inches: ";
     cin >> height_inches;
     meters= inch_to_cm(height_inches);
-    cout << "Patient is "<< meters << " cm." << endl;
+    cout << "Patient is "<< meters << " meters." << endl;
 
     cout << "Input patient weight in pounds: ";
     cin >> weight_lbs;
     kg= lb_to_kg(weight_lbs);
     cout << "Patient weighs "<< kg << " kg." << endl;
 
-    cout<< "Patient's BMI is " << patient_bmi(kg, meters);
-
+    cout<< "Patient's BMI is " << patient_bmi(kg, meters) << endl;
+    isPatientHealthy(patient_bmi(kg, meters));
     return 0;
 }
