99b20221feee9f1c13c6a3ba5677b4fe575a5b80
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-09-01 17:56:40 -0700

    Built constructor

--- HealthApp/headers/patient.h
+++ HealthApp/headers/patient.h
@@ -5,14 +5,30 @@
 #define PATIENT_H
 
 #include "human.h"
+#include "physician.h"
 #include <iostream>
 
 using namespace std;
 
 class Patient: public Human{
 public: //Should this information be private?
+    string Name;
+    int Systolic;
+    int Diastolic;
+    int RestHR;
+    float height_inches;
+    float weight_lbs;
 
+    Patient(string name, int systolic, int diastolic, int pulse, float height, float weight){
+        Name= name;
+        Systolic= systolic;
+        Diastolic= diastolic;
+        RestHR= pulse;
+        height_inches= height;
+        weight_lbs= weight;
 
+    };
+    //using Human::Human;
 
 };
 
