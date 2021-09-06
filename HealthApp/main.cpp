384750d5b1e2444d8cb276a4264eeb6c406c7e95
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-09-05 18:59:29 -0700

    Cleaned up file

--- HealthApp/main.cpp
+++ HealthApp/main.cpp
@@ -9,50 +9,9 @@
 #define patient_bmi(kg, meters)(kg/(meters * meters))
 
 using namespace std;
-
-float height_inches;
-float weight_lbs;
-float bmi;
-float meters;
-float kg;
-
-void isPatientHealthy(float bmi){
-    if (bmi >= 30){
-        cout<< "Patient is obese" << endl;
-    } else if (bmi < 30 and bmi >=25)
-    {
-        cout<< "Patient is overweight" <<endl;
-    } else if (bmi <25)
-    {
-        cout<<"Patient is healthy" << endl;
-    }
     
-    
-
-};
-int main(){
-    // cout << "Input patient height in inches: ";
-    // cin >> height_inches;
-    // meters= inch_to_cm(height_inches);
-    // cout << "Patient is "<< meters << " meters." << endl;
-
-    // cout << "Input patient weight in pounds: ";
-    // cin >> weight_lbs;
-    // kg= lb_to_kg(weight_lbs);
-    // cout << "Patient weighs "<< kg << " kg." << endl;
-
-    // cout<< "Patient's BMI is " << patient_bmi(kg, meters) << endl;
-    // isPatientHealthy(patient_bmi(kg, meters));
-
+ main(){
     Human person1("Jim", 38, "Physician");
     person1.introduction();
-
-    Human person2();
-    person2.Name = "Igor";
-    person2.introduction();
-<<<<<<< HEAD
-=======
-    
->>>>>>> main
     return 0;
 }
