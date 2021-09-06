48013cc1f3ba31a13bd121db936c165b9e9973cb
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-09-05 17:06:45 -0700

    test

--- HealthApp/headers/patient.h
+++ HealthApp/headers/patient.h
@@ -11,21 +11,14 @@
 using namespace std;
 
 class Patient: public Human{
-public: //Should this information be private?
+private: //Should this information be private?
     int Systolic;
     int Diastolic;
     int RestHR;
     float height_inches;
     float weight_lbs;
 
-    Patient(int systolic, int diastolic, int pulse, float height, float weight){
-        Systolic= systolic;
-        Diastolic= diastolic;
-        RestHR= pulse;
-        height_inches= height;
-        weight_lbs= weight;
-
-    };
+    
     
     
 
